6a22f8bc098e8b7b862e67de83efd1962986426d
Rexissu <shengruo@hotmail.com>
2025-02-19 22:30:55 +0800

    Add files via upload

--- app/api/admin-password/route.ts
+++ app/api/admin-password/route.ts
@@ -1,1 +1,6 @@
+import { NextResponse } from 'next/server';
 
+export async function GET() {
+  const adminPassword = process.env.ADMIN_PASSWORD || '';
+  return NextResponse.json({ adminPassword });
+}
