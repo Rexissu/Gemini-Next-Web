06b4721b861ff66b9d756bc7afcf6d0b162c49c1
Rexissu <shengruo@hotmail.com>
2025-02-23 00:55:33 +0800

    Add files via upload

--- lib/hooks/useInterface.ts
+++ lib/hooks/useInterface.ts
@@ -1,1 +1,11 @@
+'use client'
+import { useContext } from 'react'
+import { InterfaceContext } from '@/components/providers/InterfaceProvider'
 
+export const useInterface = () => {
+  const context = useContext(InterfaceContext)
+  if (!context) {
+    throw new Error('useInterface must be used within InterfaceProvider')
+  }
+  return context
+}
