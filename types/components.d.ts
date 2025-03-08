f224992d2d78deadd310cb30bf8e4a56b54bd5f6
Rexissu <shengruo@hotmail.com>
2025-03-08 23:44:49 +0800

    Add files via upload

--- types/components.d.ts
+++ types/components.d.ts
@@ -1,1 +1,9 @@
-/storage/emulated/0/tvboxqy/gemini-next-web - bak/types/components.d.ts
+declare module "components/media-buttons" {
+  const MediaButtons: React.ComponentType<any>;
+  export default MediaButtons;
+}
+
+declare module "components/field-item" {
+  const FieldItem: React.ComponentType<any>;
+  export default FieldItem;
+}
